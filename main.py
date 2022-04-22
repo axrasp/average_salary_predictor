e347b517d7158231134f6f6e50c2a0453350ec98
axrasp <aaraspopin@gmail.com>
2022-04-22 19:03:22 +0300

    code refactored

--- main.py
+++ main.py
@@ -28,27 +28,27 @@ def get_salary_statistics_hh(language: str):
             return vacancies_not_found
         for vacancy in found_vacancies['items']:
             if vacancy['salary']['currency'] == 'RUR':
-                if vacancy['salary']['from'] or vacancy['salary']['to']:
-                    vacancies_processed += 1
-                    total_salary += predict_rub_salary(
+                salary = predict_rub_salary(
                         payment_from=vacancy['salary']['from'],
                         payment_to=vacancy['salary']['to'])
+                if salary:
+                    vacancies_processed += 1
+                    total_salary += salary
+        if not vacancies_processed:
+            average_salary = 0
+        else:
+            average_salary = int(total_salary / vacancies_processed)
         page += 1
         page_number = found_vacancies['pages']
-    total_average_salary = int(total_salary / vacancies_processed)
     average_salary_results = {'vacancies found': found_vacancies['found'],
                               'vacancies_processed': vacancies_processed,
-                              'average_salary': total_average_salary}
+                              'average_salary': average_salary}
     return average_salary_results
 
 
 def get_salary_statistics_sj(language: str, secret_key: str):
     vacancies_processed = 0
-    average_salary = 0
     total_salary = 0
-    vacancies_not_found = {'vacancies found': 0,
-                           'vacancies_processed': 0,
-                           'average_salary': 0}
     base_api = 'https://api.superjob.ru/2.0'
     headers = {
         'X-Api-App-Id': secret_key,
@@ -62,16 +62,18 @@ def get_salary_statistics_sj(language: str, secret_key: str):
                             params=params)
     response.raise_for_status()
     found_vacancies = response.json()
-    if not found_vacancies['objects']:
-        return vacancies_not_found
     for vacancy in found_vacancies['objects']:
         if vacancy['currency'] == 'rub':
-            if vacancy['payment_from'] or vacancy['payment_to']:
+            salary = predict_rub_salary(
+                payment_from=vacancy['payment_from'],
+                payment_to=vacancy['payment_to'])
+            if salary:
                 vacancies_processed += 1
-                total_salary += predict_rub_salary(
-                    payment_from=vacancy['payment_from'],
-                    payment_to=vacancy['payment_to'])
-                average_salary = int(total_salary / vacancies_processed)
+                total_salary += salary
+    if not vacancies_processed:
+        average_salary = 0
+    else:
+        average_salary = int(total_salary / vacancies_processed)
     average_salary_results = {'vacancies found': found_vacancies['total'],
                               'vacancies_processed': vacancies_processed,
                               'average_salary': average_salary}
@@ -79,12 +81,15 @@ def get_salary_statistics_sj(language: str, secret_key: str):
 
 
 def predict_rub_salary(payment_from, payment_to):
-    if not payment_from:
-        return payment_to * 0.8
-    elif not payment_to:
-        return payment_from / 1.2
-    else:
+    if not (payment_from and payment_to):
+        if payment_from:
+            return payment_from * 1.2
+        elif payment_to:
+            return payment_to * 0.8
+    elif payment_from and payment_to:
         return (payment_from + payment_to) / 2
+    else:
+        return 0
 
 
 def print_result_table(vacancies_result: dict, title: str):
